f5176b901f3f24aaafe8baa88fc8c8f2f40faeae
Lucas Brito <britolmbs@hotmail.com>
2025-05-23 20:35:52 -0300

    corrigindo para deploy

---
+++ controllers/movieController.js
@@ -0,0 +1,20 @@
+const movieModel = require('../models/movieModel');
+
+exports.getMovies = async (req, res) => {
+    const page = req.query.page || 1;
+    try{
+        const data = await movieModel.fetchMovies(page);
+      return res.json(data);
+    }catch(err) {
+        return res.status(500).json({ erro: 'Erro ao buscar filmes.'});
+    }
+};
+
+exports.getMovieDetails = async (req, res) => {
+    try{
+        const data = await movieModel.fetchMoviesDetails(req.params.id);
+        return res.json(data);
+    }catch (err) {
+        return res.status(500).json({ error: 'Erro ao buscar detalhes do filme.'});
+    }
+};
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const movieController = require('../controllers/MovieController');
+const movieController = require('../controllers/movieController');
 
 router.get('/movies', movieController.getMovies);
 router.get('/movies/:id', movieController.getMoviesDetails);
