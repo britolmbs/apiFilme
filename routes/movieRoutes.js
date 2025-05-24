e7d74a04fa9948a9ffe70e8af2bff495629d9200
Lucas Brito <britolmbs@hotmail.com>
2025-05-24 12:39:19 -0300

    corrigindo o projeto

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const movieController = require('../controllers/movieController');
 
 router.get('/movies', movieController.getMovies);
-router.get('/movies/:id', movieController.getMoviesDetails);
+router.get('/movies/:id', movieController.getMovieDetails);
 
 module.exports = router;
